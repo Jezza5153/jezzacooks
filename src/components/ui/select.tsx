2734733faa2f08771eb6a813b93a87d69eac96af
Jeremy Arrascaeta <jezza5152@gmail.com>
2025-12-28 00:09:03 +0000

    ree up port 9002 (fixes EADDRINUSE)
    Run:
    
    lsof -i :9002
    
    You’ll see so

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -84,8 +84,7 @@ const SelectContent = React.forwardRef<
         className={cn(
           "p-1",
           position === "popper" &&
-            // ✅ FIX: width should match trigger width, NOT height
-            "w-full min-w-[var(--radix-select-trigger-width)]"
+            "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
         )}
       >
         {children}
